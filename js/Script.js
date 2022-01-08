999474eb6aed83bb8dd1cf2cc8197a70bbb9bf54
Trihrtn <@trihrtn03@gmail.com>
2022-01-08 19:43:51 +0700

    first commit

--- js/Script.js
+++ js/Script.js
@@ -1,8 +1,9 @@
 var nav = document.querySelector('nav');
 document.addEventListener('scroll', function () {
   // console.log(Hello);
-  if (window.scrollY > 80) {
+  if (window.scrollY > 100) {
     nav.style.backgroundColor = '#050417';
+    nav.style.transition = '2s';
   } else {
     nav.style.backgroundColor = 'transparent';
   }
